fad44a50ea8d0a5662dfc5d3de6ec31db646b232
timothy reynolds <timmy12011@gmail.com>
2016-10-05 11:51:43 -0600

    Initial Commit

---
+++ WiTiNa/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WiTiNa
+//
+//  Created by timothy reynolds on 10/5/16.
+//  Copyright © 2016 timothy reynolds. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
